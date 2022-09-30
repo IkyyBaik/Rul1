795f5af58aebe9e1ffa6c2e4f4cc7dc4a539491f
IkyyBaik <114123795+IkyyBaik@users.noreply.github.com>
2022-09-30 21:13:28 +0700

    Update t-sewa.js

--- plugins/t-sewa.js
+++ plugins/t-sewa.js
@@ -8,15 +8,15 @@ let handler = async (m, { conn }) => {
   title: `${await conn.getName(conn.user.jid)}`,
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
-1. Grup / 30 Hari
-Rp. 15,000 Dana
-Rp. 20,000 Pulsa
-2. Premium / 30 Hari
-Rp. 10,000 Dana
-Rp. 15,000 Pulsa
-3. Premium + Grup / 30 Hari
-Rp. 20,000 Dana
-Rp. 25,000 Pulsa
+1. Grup / 7 Hari
+Rp. 3,000 Gopay
+Rp. 5,000 Gopay
+2. Premium / 7 Hari
+Rp. 5,000 Gopay
+Rp. 8,000 Gopay
+3. Premium + Grup / Permanen
+Rp. 10,000 Gopay
+Rp. 10,000 Gopay
 wa.me/${owner[0]}
 *Bukan Bot!!!*
 *Owner ${conn.user.name}*
