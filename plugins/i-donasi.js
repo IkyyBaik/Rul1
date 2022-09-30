7c3f5361857feb4fd7e7ef2be71bc9bf70036e01
IkyyBaik <114123795+IkyyBaik@users.noreply.github.com>
2022-09-30 21:17:47 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -7,14 +7,14 @@ let handler = async (m, { conn, usedPrefix }) => {
       noteMessage: {
       extendedTextMessage: {
       text: `
-╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
-│ • Saweria  [https://saweria.co/arullofc]
+╭─「 Donasi • AllPay 」
+│ • 3 [Sqan Pp Bot]
+│ • Gopay  [0895622436731]
+│ • Dana  [Sqan Pp Bot]
+│ • Saweria  [GakAda!!]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/62895622436731
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
